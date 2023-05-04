b5bb884c4587a36913549ab8dc0fa10e2e90ba9d
Pawel-12 <122164162+Pawel-12@users.noreply.github.com>
2023-05-04 23:41:28 +0200

    Add Windows support for clearing terminal

--- src/util/util.cpp
+++ src/util/util.cpp
@@ -4,7 +4,12 @@
 
 void clearScreen()
 {
-    system("clear");
+    #if _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
+
     std::cout << "-----------" << std::endl;
     //std::cout << "\033[2K\033[1;1H";
 }
