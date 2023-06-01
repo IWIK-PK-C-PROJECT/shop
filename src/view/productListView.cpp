2e3f5ec2f95c7b02a253e579209d2f46f3e7640b
Herek <herekkacper@gmail.com>
2023-06-01 20:52:10 +0200

    Added way to get back to main menu from products

--- src/view/productListView.cpp
+++ src/view/productListView.cpp
@@ -16,5 +16,13 @@ View* ProductListView::display(session& session)
             std::cout << product.name << " / ";
             std::cout << product.price << std::endl;
         }
+    std::cout<<"1. Buy product"<<std::endl;
+    std::cout<<"2. Back to main menu"<<std::endl;
+    int choice;
+    std::cin >> choice;
+    if (choice == 1){
+        //kup
+    }else
+        return new LoggedMainView();
 }
 
