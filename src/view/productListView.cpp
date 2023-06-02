222089d4611636fb02ccab8ee60778969ba1147e
Pawel-12 <122164162+Pawel-12@users.noreply.github.com>
2023-06-03 01:34:41 +0200

    Formatting

--- src/view/productListView.cpp
+++ src/view/productListView.cpp
@@ -14,32 +14,36 @@ View* ProductListView::display(Session& session)
     {
         if (product.status != Product::Status::Available)
             continue;
-        std::cout << product.name << " / ";
-        std::cout << product.price << std::endl;
+
+        std::cout << "- " << product.name << " | "<< product.price << " USD" << std::endl;
     }
 
-    std::cout<<"1. Buy product"<<std::endl;
-    std::cout<<"2. Back to main menu"<<std::endl;
+    std::cout << "1. Buy product" << std::endl;
+    std::cout << "2. Back to main menu" << std::endl;
 
     int choice;
     std::cin >> choice;
 
-    if (choice == 1){
+    if (choice == 1)
+    {
         std::string productToBuy;
-        std::cout<<"Choose a product (type its name)"<<std::endl;
+
+        std::cout << "Choose a product (type its name)" << std::endl;
         std::cin >> productToBuy;
+
         for(Product& product : session.database.getAllProduct() )
         {
-             if (productToBuy == product.name) {
+             if (productToBuy == product.name)
+             {
                  product.status = Product::Status::Preparation;
                  return new ProductListView();
              }
         }
         std::cout << "Didnt find matching product, press anything to continue." <<std::endl;
-        std::string temp;
-        std::cin >> temp;
-        return new ProductListView();
+        std::cin.get();
+        std::cin.get();
 
+        return new ProductListView();
     }
     else
         return new LoggedMainView();
